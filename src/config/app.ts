5b720531cb5abd18ce882d1e6e371c275854faa3
zimkha <kndiaye@jaba.store>
2021-01-15 01:54:40 +0000

    implementation du code server.ts

--- src/config/app.ts
+++ src/config/app.ts
@@ -3,12 +3,15 @@ import config from 'config';
 import consola from 'consola';
 import helmet from 'helmet'
 import mongoose from 'mongoose';
+import {ApolloServer} from 'apollo-server-express';
+import typeDefs from '../graphql/schema/typedefs';
+import resolvers from '../graphql/resolvers/index'; 
 
 const env = config.get('env') as string;
 const port = config.get('PORT') as number;
 const db = config.get('MONGO_URL') as string;
 export function start(){
-    const app: Application = express();
+   const app: Application = express();
     try{
         if(env==="production"){
             app.use(helmet());
@@ -29,7 +32,10 @@ export function start(){
             }
           }
           connectDB();
-        app.listen(port,()=>consola.info(`Server running on port ${port}`))
+        
+          const server = new ApolloServer({ typeDefs,  resolvers: resolvers as any  });
+          // server.getMiddleware{}
+       // app.listen(port,()=>consola.info(`Server running on port ${port}`))
     }catch(e){
         throw new Error(e.message);
     }
